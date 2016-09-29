6d7ec12766af82d9d69009923b702a26ccd5a62c
Hugh Perkins <hughperkins@gmail.com>
2016-09-30 00:23:59 +0100

    fix following github update

--- webservice/scangitstars.py
+++ webservice/scangitstars.py
@@ -1,38 +1,47 @@
 #!/home/ubuntu/env/bin/python
 import requests
-import sys
-import os
 import yaml
 from os import path
 from os.path import join
-from docopt import docopt
 from pyquery import PyQuery
 
 script_dir = path.dirname(path.realpath(__file__))
 with open(join(script_dir, 'config.yaml'), 'r') as f:
-  config = yaml.load(f)
+               config = yaml.load(f)
 
-r = requests.get('https://github.com/%s?tab=repositories' % config['github_username'])
-c = r.content
-
-print('Content-type: text/plain')
-print('')
-d = PyQuery(c)
-d = d('.repo-list-item')
+page = 1
+nodesfound = True
 countByName = {}
-for thing in d:
-  repo_name = PyQuery(thing)('.repo-list-name')('a').text().lower()
-  count = 0
-  for statItem in PyQuery(thing)('.repo-list-stat-item'):
-    si = PyQuery(statItem)
-    href = si.attr('href')
-    if href.endswith('/stargazers'):
-      count = int(si.text())
-  if count > 0:
-    countByName[repo_name] = count
+while nodesfound:
+    # print('page', page)
+    nodesfound = False
+    r = requests.get('https://github.com/%s?tab=repositories&page=%s' % (config['github_username'], page))
+    c = r.content
+
+    # print('Content-type: text/plain')
+    # print('')
+    d = PyQuery(c)
+    # d = d('.repo-list-item')
+    d = d('.source')
+    for thing in d:
+        nodesfound = True
+        # print('thing', thing)
+        repo_name = PyQuery(thing)('.d-table')('.d-table-cell')('.f4')('a').text().lower()
+        # print('repo_name', repo_name)
+        count = 0
+        countanode = PyQuery(thing)('.d-table')('.col-1')('a')
+        # print('countanode', countanode)
+        count = int(countanode.text())
+        # si = PyQuery(statItem)
+        # href = si.attr('href')
+        # if href.endswith('/stargazers'):
+        #   count = int(si.text())
+        # print('count', count)
+        if count > 0:
+            countByName[repo_name] = count
+    page += 1
 
 sortedKeys = list(countByName.keys())
 sortedKeys.sort()
 for name in sortedKeys:
-  print(name, countByName[name])
-
+    print(name, countByName[name])
